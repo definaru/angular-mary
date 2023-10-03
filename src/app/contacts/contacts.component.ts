3a26d3e08a6384d4581504a64d1ea392b4527ab5
Alex Ander <aleksa.prohorova.art@gmail.com>
2023-10-03 23:33:44 +0300

    added page of contact

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,12 +1,18 @@
 import { Title } from '@angular/platform-browser'
 import { Inject, Component } from '@angular/core'
+import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import { heroDevicePhoneMobile, heroEnvelopeOpen, heroMapPin } from '@ng-icons/heroicons/outline'
 import { DOCUMENT } from '@angular/common'
 
 
 @Component({
+    standalone: true,
+    imports: [NgIconComponent],
+    providers: [provideIcons({ heroDevicePhoneMobile, heroEnvelopeOpen, heroMapPin })],
     selector: 'app-contacts',
     templateUrl: './contacts.component.html',
     styleUrls: ['./contacts.component.css']
+    // heroLanguage
 })
 export class ContactsComponent {
     constructor(@Inject(DOCUMENT) private document: Document, title: Title) {
